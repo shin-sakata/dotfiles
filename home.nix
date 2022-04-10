c807502aa6eb37036c1e2ed2f14cead30ef9b84e
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2022-04-11 07:27:46 +0900

    remove direnv hook & add init nix-daemon settings

--- home.nix
+++ home.nix
@@ -76,7 +76,10 @@
     enableCompletion = true;
     enableAutosuggestions = true;
     initExtra = ''
-      eval "$(direnv hook zsh)"
+      # nix daemon の起動
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
     '';
   };
 }
