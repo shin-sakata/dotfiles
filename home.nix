6be66fed99652fbf43fbf99050afb2eced33cea2
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2022-04-13 15:00:06 +0900

    change git editor.

--- home.nix
+++ home.nix
@@ -35,6 +35,9 @@
     enable = true;
     userName = "shin-sakata";
     userEmail = "shintaro.sakata.tokyo@gmail.com";
+    extraConfig = {
+      core.editor = "codium --wait";
+    };
     ignores = [
       ".direnv"
     ];
