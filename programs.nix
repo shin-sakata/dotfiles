f858d3cd1e8454d06881fd0b8ee072f266b06af6
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2022-04-18 12:22:19 +0900

    add comments

--- programs.nix
+++ programs.nix
@@ -1,8 +1,14 @@
-# programs ディレクトリの中身を読み取って良い感じのレコードにしてくれる関数
+# programs ディレクトリの中身を読み取って、
+# fileの名前をレコードのキー, fileの中身を値にして返すutil関数
+# pkgs, lib をfileの中身に渡す処理も含まれている
 { pkgs, lib }:
 let
-  # ./programs/ 配下のファイルの .nix suffix を削除したリスト
-  fileNames = lib.attrsets.mapAttrsToList (name: value: lib.strings.removeSuffix ".nix" name) (builtins.readDir ./programs);
-  mkField = name: { name = name; value = import ./programs/${name}.nix { inherit pkgs lib; };};
+  # programs ファイルが含まれる path の設定
+  programsDirPath = ./programs;
+  # programs ファイルに渡したいパラメータの設定
+  parameters = { inherit pkgs lib; };
+
+  fileNames = lib.attrsets.mapAttrsToList (name: value: lib.strings.removeSuffix ".nix" name) (builtins.readDir programsDirPath);
+  mkField = name: { name = name; value = import /${programsDirPath}/${name}.nix parameters; };
 in
 builtins.listToAttrs (map mkField fileNames)
