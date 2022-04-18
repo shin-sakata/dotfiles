1f48c03d167d507ac0c21f3ebe87909950c18ee4
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2022-04-18 13:49:18 +0900

    add sessionVariables.

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -3,6 +3,13 @@
   enable = true;
   enableCompletion = true;
   enableAutosuggestions = true;
+
+  sessionVariables = {
+    # for awscli & saml2aws
+    AWS_DEFAULT_REGION = "ap-northeast-1";
+    AWS_PROFILE = "saml";
+  };
+
   initExtra = ''
     # nix daemon の起動
     if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
