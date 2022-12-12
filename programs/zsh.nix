d545c79d8d9814b98d71fce73fc6eef313f85319
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2022-12-12 13:50:55 +0900

    refactoring

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -8,6 +8,7 @@
     # for awscli & saml2aws
     AWS_DEFAULT_REGION = "ap-northeast-1";
     AWS_PROFILE = "saml";
+    NIX_PATH = "$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels\${NIX_PATH:+:$NIX_PATH}";
   };
 
   initExtra = ''
@@ -15,7 +16,5 @@
     if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
       . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
     fi
-
-    export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels''${NIX_PATH:+:$NIX_PATH}
   '';
 }
