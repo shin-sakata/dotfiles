b1541da970f0bba3edd5b9da00d797239deebfb1
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2023-01-22 17:13:11 +0900

    remove AWS_PROFILE

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -9,6 +9,5 @@
     # niv で private repository を利用するのに必要
     # https://github.com/nmattia/niv#2-use-the-netrc-file
     GITHUB_TOKEN = "$(gh auth token)";
-    AWS_PROFILE = "dev28685203307";
   };
 }
