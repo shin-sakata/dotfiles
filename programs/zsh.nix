9588b7fef0744e5872255a02954e3291bfaa44f5
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2023-04-15 18:01:05 +0900

    修正

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -3,10 +3,4 @@
   enable = true;
   enableCompletion = true;
   enableAutosuggestions = true;
-
-  sessionVariables = {
-    # niv で private repository を利用するのに必要
-    # https://github.com/nmattia/niv#2-use-the-netrc-file
-    GITHUB_TOKEN = "$(gh auth token)";
-  };
 }
