0699452375fb398beb03100968c9ff1c2720930d
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2023-04-21 13:05:58 +0900

    fix shell init

--- home.nix
+++ home.nix
@@ -43,10 +43,4 @@
     #   org.gradle.daemon.idletimeout=3600000
     # '';
   };
-
-  home.sessionVariables = {
-    # niv で private repository を利用するのに必要
-    # https://github.com/nmattia/niv#2-use-the-netrc-file
-    GITHUB_TOKEN = "$(gh auth token)";
-  };
 }
--- programs/zsh.nix
+++ programs/zsh.nix
@@ -3,4 +3,10 @@
   enable = true;
   enableCompletion = true;
   enableAutosuggestions = true;
+
+  initExtra = ''
+    # niv で private repository を利用するのに必要
+    # https://github.com/nmattia/niv#2-use-the-netrc-file
+    export GITHUB_TOKEN=$(gh auth token)
+  '';
 }
