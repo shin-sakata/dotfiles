f47743f934d2f57fd1532c54c127c10e883ba2d0
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2024-01-17 12:46:43 +0900

    flake化

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Home Manager configuration of shin";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    codex.url = "github:herp-inc/codex";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, codex, home-manager, ... }:
+    let
+      system = "x86_64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."shin" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./home.nix
+          codex.hmModule.${system}({...}: {
+            codex.enable = true;
+          })
+        ];
+      };
+    };
+}
--- home.nix
+++ home.nix
@@ -1,45 +1,20 @@
 { config, pkgs, lib, ... }:
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "shin";
   home.homeDirectory = "/Users/shin";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "23.05";
+  home.stateVersion = "24.05";
   nixpkgs.config.allowUnfree = true;
-
-  imports = [ (import <codex>).hmModule.${builtins.currentSystem} ];
-  codex.enable = true;
 
   home.packages = [
     pkgs.direnv
     pkgs.zsh
     pkgs.jetbrains-mono # for vscode
     pkgs.niv
+    pkgs.cachix
   ];
 
   programs = import ./programs.nix { inherit pkgs lib; };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+  home.file = {};
 }
