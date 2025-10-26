32ab4dcdc8bd0ca57ece0212c9becd97b6a45dba
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2025-10-26 20:52:04 +0900

    add path

--- zsh/initExtra.zsh
+++ zsh/initExtra.zsh
@@ -2,3 +2,6 @@ eval "$(codex configure zsh)"
 # niv で private repository を利用するのに必要 niv 使わないのでもういらないかも
 # https://github.com/nmattia/niv#2-use-the-netrc-file
 # export GITHUB_TOKEN=$(gh auth token)
+
+# Android Studio コマンドライン起動用パスを追加
+export PATH="$PATH:/Applications/Android Studio.app/Contents/MacOS"
